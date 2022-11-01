94602cbebd006525082b6439ab41018a95ecc9c0
Luke Sheppard <lukesheppard@gmail.com>
2022-11-01 19:45:35 -0400

    Updated the src/script.ts file to have the script add a new link into the database and then return all the current links in the database.

--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,16 @@
+// test query the database
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 async function main() {
+  const newLink = await prisma.link.create({
+    data: {
+      description:
+        "A repo that contains an ongoing collection of useful, cleever, solidity/EVM patterns that actually get used in the wild",
+      url: "https://github.com/dragonfly-xyz/useful-solidity-patterns",
+    },
+  });
   const allLinks = await prisma.link.findMany();
   console.log(allLinks);
 }
