cde876976d7132dce3fd9bc678e30d1a60b9c858
Luke Sheppard <lukesheppard@gmail.com>
2022-11-01 18:11:53 -0400

    Added Dummy data for a feed query that returns a list of Links

---
+++ src/graphql/LinksDummyData.ts
@@ -0,0 +1,19 @@
+import { NexusGenObjects } from "../../nexus-typepgen";
+
+let links: NexusGenObjects["Link"][] = [
+  {
+    id: 1,
+    url: "https://github.com/matter-labs/awesome-zero-knowledge-proofs?fbclid=IwAR2m9q6KrwS5ynUgy51MrPLrPdyIhXz1YRwQjF5ZC779idJ9nXIwHVCJHUs#courses",
+    description:
+      "A Github Repo put together by Matter Labs to help new students learn more about zero knowledge proofs (zkp)",
+  },
+
+  {
+    id: 2,
+    url: "https://www.techinterviewhandbook.org/",
+    description:
+      "Website dedicated to help prospective software engineers prepare for technical interviews",
+  },
+];
+
+export default links;
